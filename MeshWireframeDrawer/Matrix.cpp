04f47e38c29e02c6d5e2351483d95b55f25b1573
Landwere <coryarnett12@yahoo.co.uk>
2022-04-15 11:27:36 +0100

    adjusted folder structure, removed old labs
    old labs can now be found in the legacy branch

--- MeshWireframeDrawer/Matrix.cpp
+++
@@ -1,8 +0,0 @@
-#include "Matrix.h"
-#include <math.h>
-
-
-
-
-
-
---
+++ code/Ray-Tracer/Ray-Tracer/Ray-Tracer/common.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+
+//usings
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+//constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.141592653589793285;
+
+//utility functions
+inline double degrees_to_radians(double degrees) { return degrees * pi / 180.0; }
+
+inline double random_double()
+{
+	//returns a random real in [0,1].
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+	//returns a random real in [min, max].
+	return min + (max - min) * random_double();
+}
+
+
+//common headers 
+#include "ray.h"
+#include "geometry.h"
+
+
+
+
+
